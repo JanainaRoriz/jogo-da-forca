5e08921924d497dd85291f0c35098ffa542492f5
JanainaRoriz <janainalimar@gmail.com>
2022-07-13 17:52:20 -0300

    defini duas funções e um while

--- jogo-da-forca.py
+++ jogo-da-forca.py
@@ -3,4 +3,15 @@ from palavras import palavras
 
 def palavra_valida(palavras):
     palavra = random.choice(palavras)  #esse comando vai fazer a escolha das palavras aleatoriamente
+    while '-' in palavra or ' ' in palavra:
+        palavra = random.choice(palavras)
+
+    return palavra.upper()
+
+def forca():
+    palavra = palavra_valida(palavras)
+    letras = set(palavra)  # letras das palavras
+
+
+
 
