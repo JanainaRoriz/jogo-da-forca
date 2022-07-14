82554090957d7ea91e8466c8b883b160aacd6c59
JanainaRoriz <janainalimar@gmail.com>
2022-07-14 16:17:44 -0300

    adicionado mais algunas linhas no código

--- jogo-da-forca.py
+++ jogo-da-forca.py
@@ -1,5 +1,6 @@
 import random
 from palavras import palavras
+import string
 
 def palavra_valida(palavras):
     palavra = random.choice(palavras)  #esse comando vai fazer a escolha das palavras aleatoriamente
@@ -11,6 +12,13 @@ def palavra_valida(palavras):
 def forca():
     palavra = palavra_valida(palavras)
     letras = set(palavra)  # letras das palavras
+    alfabeto = set(string.ascii_uppercase)
+    letras_usadas = set()   #o que o usuário adivinhou
+
+    # #pegando o input do usuário
+
+
+
 
 
 
